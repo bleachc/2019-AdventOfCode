95b2dbb742dff1e9aae6b2b07097a1b531aaed50
Ben Leach <benleach@pm.me>
2020-12-06 23:28:28 -0500

    Day 3: Part 2

--- 03/main.ts
+++ 03/main.ts
@@ -13,45 +13,67 @@ const getInstructions = (directions: string): [string, number] => {
     return [matches[1], parseInt(matches[2])]
 }
 
-const getNewPosition = (pos: Point, {x, y}: Velocity): [number, number] => x
+const getNewPosition = (pos: Point, {x, y}: Velocity): Point => x
     ? [pos[0] + x, pos[1]]
     : [pos[0], pos[1] + y]
 
-const generatePoints = (pos: Point, {x, y}: Velocity) => x
-    ? range(Math.abs(x)).map(s => `${pos[0] + (s * (x > 0 ? 1 : -1))},${pos[1]}`)
-    : range(Math.abs(y)).map(s => `${pos[0]},${pos[1] + (s * (y > 0 ? 1 : -1))}`)
+const generatePoints = (pos: Point, {x, y}: Velocity): Point[] => x
+    ? range(Math.abs(x)).map(s => [pos[0] + (s * (x > 0 ? 1 : -1)), pos[1]])
+    : range(Math.abs(y)).map(s => [pos[0], pos[1] + (s * (y > 0 ? 1 : -1))])
 
 const range = (x) => new Array(x).fill(0).map((_, i) => i + 1)
 
-const getCoordinates = (wire: string[], i: number = 0, pos: [number, number] = [0, 0], aggregate: Set<string> = null): Set<string> => {
-    if (!aggregate) aggregate = new Set()
+const getCoordinates = (wire: string[], i: number = 0, pos: [number, number] = [0, 0], aggregate: Point[] = []): Point[] => {
+    if (!aggregate) aggregate = [] as Point[]
     if (i > wire.length || !wire[i]) return aggregate
 
     const [direction, steps] = getInstructions(wire[i])
 
     switch (direction) {
         case 'U':
-            for (let point of generatePoints(pos, {x: steps})) aggregate.add(point)
-            return getCoordinates(wire, i + 1, getNewPosition(pos, {x: steps}), aggregate)
+            return getCoordinates(wire, i + 1, getNewPosition(pos, {x: steps}), [...aggregate, ...generatePoints(pos, {x: steps})])
         case 'D':
-            for (let point of generatePoints(pos, {x: -steps})) aggregate.add(point)
-            return getCoordinates(wire, i + 1, getNewPosition(pos, {x: -steps}), aggregate)
+            return getCoordinates(wire, i + 1, getNewPosition(pos, {x: -steps}), [...aggregate, ...generatePoints(pos, {x: -steps})])
         case 'R':
-            for (let point of generatePoints(pos, {y: steps})) aggregate.add(point)
-            return getCoordinates(wire, i + 1, getNewPosition(pos, {y: steps}), aggregate)
+            return getCoordinates(wire, i + 1, getNewPosition(pos, {y: steps}), [...aggregate, ...generatePoints(pos, {y: steps})])
         case 'L':
-            for (let point of generatePoints(pos, {y: -steps})) aggregate.add(point)
-            return getCoordinates(wire, i + 1, getNewPosition(pos, {y: -steps}), aggregate)
+            return getCoordinates(wire, i + 1, getNewPosition(pos, {y: -steps}), [...aggregate, ...generatePoints(pos, {y: -steps})])
     }
 }
 
-const calculateManhattanDistance = (point: string) => {
+const toPoint = (point: string): Point => {
     const [x, y] = point.split(',').map(x => parseInt(x))
+    return [x, y]
+}
+
+const calculateManhattanDistance = (point: string) => {
+    const [x, y] = toPoint(point)
     return Math.abs(x) + Math.abs(y)
 }
 
-const wiresCoordinates = wires.map(w => getCoordinates(w))
-const intersections = new Set(Array.from(wiresCoordinates[0]).filter(x => wiresCoordinates[1].has(x)))
-const distances = Array.from(intersections).map(x => calculateManhattanDistance(x))
+const wirePaths = wires.map(w => getCoordinates(w))
+const visited = wirePaths.map(_ => new Set(_.map(([x, y]) => `${x},${y}`)))
+const intersections = new Set(Array.from(visited[0]).filter(x => visited[1].has(x)))
+const intersectionDistances = Array.from(intersections).map(x => calculateManhattanDistance(x))
 
-console.log('Part 1', Math.min(...distances))
+console.log('Part 1', Math.min(...intersectionDistances))
+
+// Solution if recursion depth not exceeded
+// const calculateStepDistance = ([x, y]: Point, path: Point[], i = 0): number => x === path[i][0] && y === path[i][1]
+//     ? i
+//     : calculateStepDistance([x, y], path, i + 1)
+// const stepDistances = Array.from(intersections).map(toPoint).map(p => calculateStepDistance(p, wirePaths[0]) + calculateStepDistance(p, wirePaths[1]))
+
+const calculateStepDistance = (target: Point, path: Point[]): number => {
+    const [x, y] = target
+    for (let i = 0; i < path.length; i++) if (path[i][0] === x && path[i][1] === y) return i + 1
+    throw new Error('Target not found')
+}
+
+const stepDistances = Array.from(intersections).map((intersect) => {
+    const target = toPoint(intersect)
+    const distances = wirePaths.map(path => calculateStepDistance(target, path))
+    return distances.reduce((acc, val) => acc + val)
+})
+
+console.log('Part 2', Math.min(...stepDistances))
